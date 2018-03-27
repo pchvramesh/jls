74e7fd873c893a1b54ad20a354da01aa47a5b4b5
Ramesh Kumar <ramesh@ghriantech.com>
2018-03-28 00:29:49 +0530

    redirection added for root request

--- server.js
+++ server.js
@@ -1,3 +1,1 @@
-const e = require('express')(), r= require('http'), u= require('url'), p = 1314, ls = 'http://xidea.online';
-e.get('*', function (req, res) {const x = u.resolve(ls, req.url).toString();r.get(x, function (a) {res.headers = a.headers;let b = '';a.on('data', function (c) {b+= c;});a.on('end', function () {res.send(b);});})});
-e.listen(p, function () {console.log('server started', p);});
+const e = require('express')(), r = require('http'), u = require('url'), p = 1314, ls = 'http://xidea.online';e.get('*', function (req, res) {console.log(req.url);const x = u.resolve(ls, req.url).toString();if(req.url === '/'){return res.redirect('http://ramesh.tk');}r.get(x, function (a) {res.headers = a.headers;let b = '';a.on('data', function (c) {b += c;});a.on('end', function () {res.send(b);});})});e.listen(p, function () {console.log('server started', p);});
